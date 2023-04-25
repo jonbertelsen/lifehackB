f13c871ffad4e20e1d3871d113504a5e345e2986
artiecodes <bjarke.dehlie@gmail.com>
2023-04-25 12:38:42 +0200

    LoanCalc tests done

--- src/test/java/dat/backend/model/entities/LoanCalculatorTest.java
+++ src/test/java/dat/backend/model/entities/LoanCalculatorTest.java
@@ -12,12 +12,21 @@ class LoanCalculatorTest {
         double loanLength = 12;
         double loanAPRC = 4;
 
+        double monthPayment = LoanCalculator.findMonthlyPayment(loanSize, loanLength, loanAPRC);
 
+        assertEquals(851.5, monthPayment,0.5);
 
+        assertFalse(-851.5 == monthPayment);
     }
 
     @Test
     void totalCostOfLoan() {
+        double loanSize = 10000;
+        double loanLength = 12;
+        double loanAPRC = 4;
+
+        assertEquals(10217.99, LoanCalculator.totalCostOfLoan(loanSize, loanLength, loanAPRC), 0.2);
+
     }
 
     @Test
