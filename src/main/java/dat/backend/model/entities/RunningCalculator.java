28834fb00146baef5696ff3b0e70082ae95e3981
MrJustMeDahl <nico.dahl93@gmail.com>
2023-04-25 12:58:27 +0200

    Added thrown exceptions in case of Illegal arguments input.

--- src/main/java/dat/backend/model/entities/RunningCalculator.java
+++ src/main/java/dat/backend/model/entities/RunningCalculator.java
@@ -8,17 +8,32 @@ Every method in this class is going to be static, since the class itself does no
 public class RunningCalculator {
 
     //This method calculates how long you can run (in km) - given a certain time (in minutes) and a given speed (in km. pr. hour).
-    public static double calculateDistance(int time, double speed){
+    public static double calculateDistance(int time, double speed) throws IllegalArgumentException{
+        if(time <= 0) {
+            throw new IllegalArgumentException("Time is not allowed to be zero or negative.");
+        } else if(speed <= 0){
+            throw new IllegalArgumentException("Speed is not allowed to be zero or negative.");
+        }
         return (speed * time) / 60;
     }
 
     //This method calculates how long time (in minutes) it will take to run a certain distance (in km) at a given speed (in km. pr. hour).
-    public static double calculateTime(double distance, double speed){
+    public static double calculateTime(double distance, double speed) throws IllegalArgumentException{
+        if(distance <= 0) {
+            throw new IllegalArgumentException("Distance is not allowed to be zero or negative.");
+        } else if(speed <= 0){
+            throw new IllegalArgumentException("Speed is not allowed to be zero or negative.");
+        }
         return (distance * 60) / speed;
     }
 
     //This method calculates how fast you are running (in km. pr. hour) given a distance (in km) and a timeframe (in minutes).
-    public static double calculateSpeed(double distance, int time){
+    public static double calculateSpeed(double distance, int time) throws IllegalArgumentException{
+        if(time <= 0) {
+            throw new IllegalArgumentException("Time is not allowed to be zero or negative.");
+        } else if(distance <= 0){
+            throw new IllegalArgumentException("Distance is not allowed to be zero or negative.");
+        }
         return (distance * 60) / time;
     }
 
