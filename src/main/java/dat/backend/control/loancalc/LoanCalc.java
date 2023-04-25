84355f8f2ddda6958e3fe5bfb05da833954948d3
artiecodes <bjarke.dehlie@gmail.com>
2023-04-25 11:52:18 +0200

    LoanCalc entity WIP

--- src/main/java/dat/backend/control/loancalc/LoanCalc.java
+++ src/main/java/dat/backend/control/loancalc/LoanCalc.java
@@ -21,5 +21,32 @@ public class LoanCalc extends HttpServlet {
         double APRC = Double.parseDouble(request.getParameter("APRC"));
         double duration = Double.parseDouble(request.getParameter("duration"));
 
+        if(loanAmount < 0 || APRC < 0 || duration < 0){
+            request.setAttribute("fejlinput", "You cannot input negative values!");
+            request.getRequestDispatcher("loancalc.jsp").forward(request, response);
+        }
+
+        if (request.getParameterMap().containsKey("monthYearChange")){
+            APRC = LoanCalculator.recalculateToMonths(APRC);
+        }
+
+        // is getting the total cost of the loan as it is
+        double totalCostOfLoan = LoanCalculator.totalCostOfLoan(loanAmount, duration, APRC);
+        //gets the monthlypayment
+        double monthlyPayment = LoanCalculator.findMonthlyPayment(loanAmount, duration, APRC);
+        // gets total loan if we lowers the APRC with 50%
+        double totalCostWithlowerAPRC = LoanCalculator.totalCostWithLowerAPRC(loanAmount, duration, APRC);
+        // gets total loan if we lower the loanlenght by 33%
+        double totalCostShorterDuration = LoanCalculator.totalCostWithLowerLength(loanAmount, duration, APRC);
+
+        double interest = totalCostOfLoan - loanAmount;
+        request.setAttribute("interest", interest);
+        request.setAttribute("loanlength", duration);
+        request.setAttribute("totalcostofloan", totalCostOfLoan);
+        request.setAttribute("monthlypayment", monthlyPayment);
+        request.setAttribute("totalcostwithlowerAPRC", totalCostWithlowerAPRC);
+        request.setAttribute("totalcostshorterduration", totalCostShorterDuration);
+
+        request.getRequestDispatcher("loancalc.jsp").forward(request, response);
     }
 }
