b3e2c8138ce6ee1d66c018504066a89c3c8af12a
artiecodes <bjarke.dehlie@gmail.com>
2023-04-25 12:24:53 +0200

    tests WIP

---
+++ src/test/java/dat/backend/model/entities/LoanCalculatorTest.java
@@ -0,0 +1,30 @@
+package dat.backend.model.entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoanCalculatorTest {
+
+    @Test
+    void findMonthlyPayment() {
+        double loanSize = 10000;
+        double loanLength = 12;
+        double loanAPRC = 4;
+
+
+
+    }
+
+    @Test
+    void totalCostOfLoan() {
+    }
+
+    @Test
+    void totalCostWithLowerAPRC() {
+    }
+
+    @Test
+    void totalCostWithLowerLength() {
+    }
+}
